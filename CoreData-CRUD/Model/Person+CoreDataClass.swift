79bbf8a5aa78e29e2663d54989b87c3850d970bd
Brian McIntosh <brian@mixinteractive.com>
2022-12-24 12:56:26 -0500

    Adding storyboard and Swift updates.

---
+++ CoreData-CRUD/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreData-CRUD
+//
+//  Created by Brian McIntosh on 12/24/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
--- CoreData-CRUD/ViewController.swift
+++ CoreData-CRUD/ViewController.swift
@@ -3,17 +3,50 @@
 //  CoreData-CRUD
 //
 //  Created by Brian McIntosh on 12/24/22.
-//
+//  
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Reference to managed object context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // Data for the table
+    var items: [Person]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell")
+        
+        cell?.textLabel?.text = "Placeholder"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tappity tap tap")
+    }
+    
+}
+
 
